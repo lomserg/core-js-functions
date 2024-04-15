8c0535f58df5334c7496f8a8c36fa33199b5fc89
lomserg <lomserg@gmail.com>
2024-04-15 23:35:24 +0300

    feat: add solutions

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -116,8 +116,16 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const cache = {};
+
+  return function memoizedFunction(...args) {
+    const key = JSON.stringify(args);
+    if (!(key in cache)) {
+      cache[key] = func(...args);
+    }
+    return cache[key];
+  };
 }
 
 /**
